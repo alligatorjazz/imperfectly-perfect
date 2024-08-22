0d08e667f4978558f9ae5ddb8a7a64897e37c0c7
Damian Thomas <dimitrisonic@gmail.com>
2024-08-22 12:22:53 -0400

    tooltip opacity transition added

--- components/IconButton.tsx
+++ components/IconButton.tsx
@@ -16,29 +16,28 @@ export function IconButton({ className, tooltip, children }: Props) {
 
 		// Get calculated tooltip coordinates and size
 		const rect = element.getBoundingClientRect();
-
 		// push tooltip in bounds (horizontally)
 		if (rect.left < 0) {
-			element.style.left = `calc(50% - ${rect.left}px)`
+			element.style.left = `calc(50% - ${rect.left }px)`
 		}
 		if (rect.right > window.innerWidth) {
 			element.style.left = `calc(50% - ${rect.right - window.innerWidth}px)`
 		}
-
-		// push tooltip in bounds (vertically)
-		if (rect.top < 0) {
-			element.style.top = `calc(50% - ${rect.top}px)`
-		}
-		if (rect.bottom > window.innerHeight) {
-			element.style.top = `calc(50% - ${rect.bottom - window.innerHeight}px)`
-		}
+		console.log("repositioned: ", rect);
+		// // push tooltip in bounds (vertically)
+		// if (rect.top * 2< 0) {
+		// 	element.style.top = `calc(50% - ${rect.top * 2}px)`
+		// }
+		// if (rect.bottom * 2 > window.innerHeight) {
+		// 	element.style.top = `calc(50% - ${rect.bottom * 2 - window.innerHeight}px)`
+		// }
 	}, [])
 
-
+	useEffect(reposition, []);
 	return (
-		<button className={["relative group", className].join(" ")} onMouseEnter={reposition}>
+		<button className={["relative group", className].join(" ")} onResize={reposition}>
 			{children}
-			{tooltip && <div ref={tooltipRef} className="absolute top-8 left-1/2 -translate-x-1/2 text-center p-1 hidden bg-bgColor border border-textColor font-bold text-xs whitespace-nowrap uppercase group-hover:inline">{tooltip}</div>}
+			{tooltip && <div ref={tooltipRef} className="pointer-events-none inline transition-all opacity-0  absolute top-8 left-1/2 -translate-x-1/2 text-center p-1 bg-bgColor border border-textColor font-bold text-xs whitespace-nowrap uppercase group-hover:scale-100 group-hover:opacity-100 group-hover:drop-shadow-md group-hover:pointer-events-auto">{tooltip}</div>}
 		</button>
 	);
 
