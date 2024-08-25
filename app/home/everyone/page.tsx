81d562b4d8d2316dcb43d7d3345d8c0a5db61a25
Damian Thomas <dimitrisonic@gmail.com>
2024-08-25 06:58:40 -0400

    added AllFeed to everyone tab to attempt to fix ssr hydration errors; removed dummy csvs

--- app/home/everyone/page.tsx
+++ app/home/everyone/page.tsx
@@ -1,19 +1,13 @@
 import { Metadata } from "next";
-import { getPosts } from "../../../lib/api";
-import { PostList } from "../../../components/PostList";
-import dayjs from "dayjs";
+import { AllFeed } from "../../../components/AllFeed";
 
 export const metadata: Metadata = {
 	title: 'PI.FYI | Everyone',
 	description: "A Taste of YOUR Taste"
 };
 
-export default async function Everyone() {
-	const posts = await getPosts({ limit: 25, before: dayjs().add(7, "day").toISOString(), after: dayjs().subtract(1, "month").toISOString() });
-
+export default function Everyone() {
 	return (
-		<>
-			{posts && <PostList posts={posts} next={{ limit: 10}} />}
-		</>
+		<AllFeed />
 	);
 }
--- components/AllFeed.tsx
+++ components/AllFeed.tsx
@@ -4,18 +4,19 @@ import { PostList } from "./PostList";
 import { getPosts } from '../lib/api';
 import { useEffect, useState } from "react";
 import { UserPost } from "../types";
+import dayjs from "dayjs";
 
 
-export async function WhatsHot() {
+export function AllFeed() {
 	const [initialPosts, setInitialPosts] = useState<UserPost[]>();
 	useEffect(() => {
-		getPosts({ limit: 25 })
-			.then(posts => posts ? setInitialPosts(posts) : setInitialPosts([]) )
+		getPosts({ limit: 25, before: dayjs().add(7, "day").toISOString() })
+			.then(posts => posts ? setInitialPosts(posts) : setInitialPosts([]))
 			.catch(err => console.error(err));
 	}, []);
+
 	return (
 		<div>
-			<Header level={1} className="text-5xl w-full text-center py-8">{"What's hot!"}</Header>
 			{initialPosts && <PostList posts={initialPosts} next={{ original: true, limit: 10 }} />}
 		</div>
 	);
